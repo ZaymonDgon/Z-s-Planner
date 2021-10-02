81a5f3ac2e26fcb1f51eb23000b3e087c12335af
ZaymonDgon <zaydmonkey@gmail.com>
2021-10-02 16:08:40 -0600

    implemented momment to the header.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
 // var stag = moment();
 // $('#stagtime').text(stag.format('H'));
 var today = moment();
-$("#stagtime").text(today.format("MMM Do, YYYY"));
+$("#currentDay").text(today.format("dddd MMMM Do, YYYY, h:mm:ss a"));
