daf5045ffd3c2c763bfb129c1aeee252c3843a06
ZaymonDgon <zaydmonkey@gmail.com>
2021-10-03 23:48:06 -0600

    managed to get the local storage to work with help of someone from slack

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
-// var stag = moment();
-// $('#stagtime').text(stag.format('H'));
 var today = moment();
 $("#currentDay").text(today.format("dddd MMMM Do, YYYY, h:mm:ss a"));
+
+const hour = $('.hourDiv');
+const plan = $('.text');
+const save = $('.save');
+
+input = localStorage.getItem('text')
+plan.text(input);
+
+
+save.click(function () {
+    const hour = $(this).attr("id");
+    const planner = $(this).siblings('textarea').val();
+    localStorage.setItem( hour, planner);
+    
+    
+    // plan.textContent = input
+});
